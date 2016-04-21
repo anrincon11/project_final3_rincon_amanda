5d60bba8ec7863ca0fe45bb718cb58662b54a3f2
Amanda Rincon <anrincon@ufl.edu>
2016-04-20 17:22:27 -0700

    add custom javascript code to project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 jQuery( document ).ready( function() {
 
-	// MEAN MENU PLUGIN //
+	// mean menu plugin //
 	$( 'header nav' ).meanmenu();
 
-	// TEXTILATE PLUGIN //
+	// textilate plugin //
 	$( '.tlt' ).textillate();
 
+} );
+
+// custom javascript code //
+
+//button for egift cards//
+var button = document.getElementById( 'button1' );
+
+button.addEventListener( 'click', function( event ) {
+	event.preventDefault();
+
+	var myElement = document.createElement( 'p' );
+
+	myElement.innerHTML = '<p>Thank you for your purchase!</p>';
+	document.getElementsByClassName( 'balance1' )[ 0 ].appendChild( myElement );
+} );
+
+//button for traditional gift cards//
+var button = document.getElementById( 'button2' );
+
+button.addEventListener( 'click', function( event ) {
+	event.preventDefault();
+
+	var myElement = document.createElement( 'p' );
+
+	myElement.innerHTML = '<p>Thank you for your purchase!</p>';
+	document.getElementsByClassName( 'balance2' )[ 0 ].appendChild( myElement );
 } );
